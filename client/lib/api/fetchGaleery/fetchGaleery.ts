f6afb44f11b7b5ecdd758a237f4aba8e35d25d08
Marian Narozhniak <narozhniakmarian@gmail.com>
2025-11-22 04:47:02 +0100

    11

--- client/lib/api/fetchGaleery/fetchGaleery.ts
+++ client/lib/api/fetchGaleery/fetchGaleery.ts
@@ -1,21 +1,17 @@
-import apiUrl from '../api';
-import { Gallery } from '../../types/type';
-import axios from 'axios';
-
+import apiUrl from "../api";
+import { Gallery } from "../../types/type";
+import axios from "axios";
 
 export interface GalleryResponse {
-    data: Gallery[];
+  data: Gallery[];
 }
 
 async function fetchGallery(): Promise<GalleryResponse> {
   const galleryUrl = `${apiUrl}/gallery`;
-
+  console.log(galleryUrl);
   const response = await axios.get<GalleryResponse>(galleryUrl);
-    console.log("Raw response:", response.data);
+  console.log("Raw response:", response.data);
 
   return response.data;
 }
 export default fetchGallery;
-
-
-
--- client/lib/api/fetchStore/fetchStore.ts
+++ client/lib/api/fetchStore/fetchStore.ts
@@ -1,6 +1,7 @@
 import { Product } from "@/lib/types/type";
 import apiUrl from "../api";
 import axios from "axios";
+import { log } from "console";
 
 export interface StoreResponse {
   data: Product[];
@@ -8,7 +9,7 @@ export interface StoreResponse {
 
 async function fetchStore(): Promise<StoreResponse> {
   const storeUrl = `${apiUrl}/store`;
-
+  console.log(storeUrl);
   const response = await axios.get<StoreResponse>(storeUrl);
   console.log("Raw response:", response.data);
 
